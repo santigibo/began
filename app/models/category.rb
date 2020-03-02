7589c6329b00077fb935d57d19d749a7a9ceb30e
Felix Timmel <f.timmel95@gmail.com>
2020-03-02 17:32:30 -0500

    adding validation and dependencies

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  has_many :users
+  has_many :challenges
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :challenge_recipes
+  has_many :challenges, through: :challenge_recipes
+
+  has_many :user_recipes
+  has_many :users, through: :user_recipes
 end
