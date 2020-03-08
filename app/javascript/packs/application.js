00d1b03421847e5a0ddad021723c517938cba110
Felix Timmel <f.timmel95@gmail.com>
2020-03-08 15:45:02 -0400

    adding bubble for comment and trying to implement a JS methode to toggle the form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import { showExplanation } from "../plugins/questions"
 import { iconToggle } from "../plugins/heart-toggle"
+import { form_toggle } from "../plugins/comment-form-toggle"
 
 showExplanation();
 iconToggle();
