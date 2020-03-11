50ee85fe55953b415c3d99c891d784cb9fab72a3
Vicky Andre <v_key13@hotmail.com>
2020-03-11 13:19:29 -0400

    checkbox of quizz

--- app/javascript/plugins/questions.js
+++ app/javascript/plugins/questions.js
@@ -1,10 +1,18 @@
 const showExplanation = () => {
   const answers = document.querySelectorAll('.answer');
   answers.forEach((answer) => {
-    answer.querySelector('.form-check').addEventListener('click', (event) => {
-      event.preventDefault();
+    answer.querySelector('.form-check .form-check-input').addEventListener('click', (event) => {
+      // event.preventDefault();
+      // const checkbox = event.currentTarget.firstElementChild
+      // checkbox.disabled = true;
       const explanation = answer.querySelector('.explanation')
-      explanation.classList.toggle('d-none');
+      explanation.classList.toggle('d-none')
+      // if (checkbox.checked === true) {
+      //   checkbox.checked = false;
+      // } else {
+      //   checkbox.checked = true;
+      // }
+
     });
   });
 };
