4c53f0540e2c5e627e426201accd878cad23313c
Felix Timmel <f.timmel95@gmail.com>
2020-03-12 18:47:53 -0400

    styling the forum

--- app/javascript/plugins/comment-form-toggle.js
+++ app/javascript/plugins/comment-form-toggle.js
@@ -4,7 +4,7 @@ const form_toggle = () => {
   const post_sections = document.querySelectorAll('.post_partie')
 
   post_sections.forEach((post_section) => {
-      const btn = post_section.querySelector('.fa-comments')
+      const btn = post_section.querySelector('.comment_toggle')
 
     btn.addEventListener('click', () => {
       const comment = post_section.querySelector('.comment_part');
