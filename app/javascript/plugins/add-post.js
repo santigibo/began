d44781d5e675c6aace128dfd89954ce76a053df1
Felix Timmel <f.timmel95@gmail.com>
2020-03-12 14:45:20 -0400

    adding a cross to close the add form

--- app/javascript/plugins/add-post.js
+++ app/javascript/plugins/add-post.js
@@ -2,9 +2,18 @@ const post_form_toggle = () => {
 
   const new_post = document.getElementById('add_a_post')
 
-  new_post.addEventListener('click', () => {
+  new_post.addEventListener('click', (event) => {
+    event.currentTarget.classList.add('d-none')
     const post_form = document.querySelector('.form_part');
     post_form.classList.remove('d-none');
+  });
+
+  const cross = document.getElementById('close')
+
+  cross.addEventListener('click', () => {
+    new_post.classList.remove('d-none')
+    const post_form = document.querySelector('.form_part');
+    post_form.classList.add('d-none');
   });
 }
 
