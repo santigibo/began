b4c5b0f123dc3a0712743144b9eb5020fa3e87bd
Vicky Andre <v_key13@hotmail.com>
2020-03-12 18:54:00 -0400

    quizz: added logic to click on answers, show wrong answers as striked

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,13 +5,16 @@ import { form_toggle } from "../plugins/comment-form-toggle"
 import { cookbook_card_toggle } from "../plugins/heart-cookbook-toggle"
 import { post_form_toggle } from "../plugins/add-post"
 import { dragg } from "../plugins/draggable-item"
+import { striking } from "../plugins/striking"
 
 showExplanation();
+striking();
 iconToggle();
 form_toggle();
 cookbook_card_toggle();
 post_form_toggle();
 dragg();
+
 
 $('#diet-carousel').carousel({
   interval: false
