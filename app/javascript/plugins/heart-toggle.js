aec4011fe4ca65eacecba8f68b7593ac01cecd6c
Felix Timmel <f.timmel95@gmail.com>
2020-03-12 11:03:21 -0400

    styling the alerte and display them when adding or removing recipe

--- app/javascript/plugins/heart-toggle.js
+++ app/javascript/plugins/heart-toggle.js
@@ -4,6 +4,21 @@ const iconToggle = () => {
     icon.addEventListener( 'click', (event) => {
       event.currentTarget.classList.toggle('fas');
       event.currentTarget.classList.toggle('far');
+
+      const added_notif = document.querySelector('#add_recipe')
+      const removed_notif = document.querySelector('#remove_recipe')
+
+      if (icon.classList.contains('far')) {
+        removed_notif.classList.remove('d-none');
+        setTimeout(function(){
+          removed_notif.classList.add('d-none');
+        }, 2500);
+      } else {
+        added_notif.classList.remove('d-none');
+        setTimeout(function(){
+          added_notif.classList.add('d-none');
+        }, 2500);
+      }
     });
   });
 };
